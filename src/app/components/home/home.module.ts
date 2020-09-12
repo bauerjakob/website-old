1f5b33fa4d2914cb70f2eb0e5c9ad94bd4c73162
Jakob Bauer <Jakob.Bauer01@outlook.com>
2020-09-12 10:28:33 +0200

    Added footer

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,22 +2,27 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
 import { ContactMeComponent } from './contact-me/contact-me.component'
+import { FooterComponent } from './footer/footer.component'
 import { MyProjectsService } from '@services/my-projects.service'
 import { ContactMeService } from '@services/contact-me.service'
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [
         SharedModule,
-        CommonModule
+        CommonModule,
+        RouterModule
     ],
     exports: [
         MyProjectsComponent,
-        ContactMeComponent
+        ContactMeComponent,
+        FooterComponent
     ],
     declarations: [
         MyProjectsComponent,
-        ContactMeComponent
+        ContactMeComponent,
+        FooterComponent
     ],
     providers: [
         MyProjectsService,
