2672d5cc105f6df20801e75ee2fc19aeae18e504
Jakob Bauer <Jakob.Bauer01@outlook.com>
2020-09-12 11:24:09 +0200

    Added about me section

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -4,6 +4,7 @@ import { MyProjectsComponent } from './my-projects/my-projects.component';
 import { ContactMeComponent } from './contact-me/contact-me.component'
 import { FooterComponent } from './footer/footer.component'
 import { HeaderComponent } from './header/header.component'
+import { AboutMeComponent } from './about-me/about-me.component'
 import { MyProjectsService } from '@services/my-projects.service'
 import { ContactMeService } from '@services/contact-me.service'
 import { CommonModule } from '@angular/common';
@@ -17,12 +18,14 @@ import { RouterModule } from '@angular/router';
     ],
     exports: [
         HeaderComponent,
+        AboutMeComponent,
         MyProjectsComponent,
         ContactMeComponent,
         FooterComponent
     ],
     declarations: [
         HeaderComponent,
+        AboutMeComponent,
         MyProjectsComponent,
         ContactMeComponent,
         FooterComponent
